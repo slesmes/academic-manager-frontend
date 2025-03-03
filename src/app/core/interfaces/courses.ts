7f6a2c07c50a3c050094930725e157447c6a4209
CamiloA-17 <camiangi1517@gmail.com>
2025-03-03 17:55:33 -0500

    Finishing create and delete from cursos

--- src/app/core/interfaces/courses.ts
+++ src/app/core/interfaces/courses.ts
@@ -1,6 +1,14 @@
+import { Professor } from "./professors";
+
 export interface Course {
-    id: string;
+    id: number;
     name: string;
     description: string;
-    professor?: string;
+    professor?: Professor;
+}
+
+export interface CreateCourse {
+    name: string;
+    description: string;
+    professorId: string;
 }
--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -1,10 +1,12 @@
 import { CoursesService } from './../../core/services/courses.service';
 import { Component, OnInit } from '@angular/core';
-import { Department } from '../../core/interfaces/departments';
-import { DepartmentsService } from '../../core/services/departments.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Course } from '../../core/interfaces/courses';
+import { Course, CreateCourse } from '../../core/interfaces/courses';
+import { Schedule } from '../../core/interfaces/schedule';
+import { Prerequisite } from '../../core/interfaces/prerequisite';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-courses',
@@ -13,47 +15,97 @@ import { Course } from '../../core/interfaces/courses';
   imports: [CommonModule, FormsModule]
 })
 export class CoursesComponent implements OnInit {
-  courses: Course[] = [];
-  departments: Department[] = [];
+  cursos: Course[] = [];
+  horarios: Schedule[] = [];
+  prerequisitos: Prerequisite[] = [];
+  mostrarModal: boolean = false;
+  cursoSeleccionado: Course | null = null;
 
   mostrarFormulario: boolean = false;
   mostrarFormularioEdicion: boolean = false;
   mostrarFormularioEliminar: boolean = false;
   mostrarFormularioBuscar: boolean = false;
-  nuevoCurso: Course = { id: '', name: '', description: '', professor: '' };
-  cursoAEditar: Course = { id: '', name: '', description: '', professor: '' };
-  cursoAEliminar: Course = { id: '', name: '', description: '', professor: '' };
-  cursoABuscar: Course = { id: '', name: '', description: '', professor: '' };
+  nuevoCurso: CreateCourse = { name: '', description: '', professorId: '' };
+  cursoAEditar: Course = { id: 0, name: '', description: '', professor: { id: 0, name: '', hiredate: new Date() } };
+  cursoAEliminar: Course = { id: 0, name: '', description: '', professor: { id: 0, name: '', hiredate: new Date() } };
+  cursoABuscar: Course = { id: 0, name: '', description: '', professor: { id: 0, name: '', hiredate: new Date() } };
 
-  constructor(private courseService: CoursesService) { }
+  constructor(private courseService: CoursesService, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.cargarCursos();
+  }
+
+  cargarCursos() {
     this.courseService.getCourses().subscribe({
       next: (result) => {
-        this.courses = result;
-        console.log(this.courses);
-        
-      }
+        this.cursos = result;
+        console.log(this.cursos);
+      },
+      error: (err) => {
+        console.error('Error al cargar cursos:', err);
+      },
     });
   }
 
+  abrirModal(curso: Course): void {
+    this.cursoSeleccionado = curso;
+    this.mostrarModal = true;
+
+    this.courseService.getSchedulesByCourseId(curso.id).subscribe({
+      next: (schedules) => {
+        this.horarios = schedules;
+      },
+      error: (err) => {
+        console.error('Error al cargar horarios:', err);
+      },
+    });
+
+    this.courseService.getPreRequisitesByCourseId(curso.id).subscribe({
+      next: (prerequisites) => {
+        this.prerequisitos = prerequisites;
+        console.log(this.prerequisitos);
+
+      },
+      error: (err) => {
+        console.error('Error al cargar prerequisitos:', err);
+      },
+    });
+  }
+
+  cerrarModal(): void {
+    this.mostrarModal = false;
+    this.cursoSeleccionado = null;
+    this.horarios = [];
+    this.prerequisitos = [];
+  }
+
   crearCurso() {
-  
+
+    this.courseService.createCourse(this.nuevoCurso).subscribe({
+      next: (result) => {
+        console.log('Curso creado:', result);
+        this.cargarCursos();
+      },
+      error: (err) => {
+        console.error('Error al crear curso:', err);
+      },
+    });
   }
 
-  editarCurso(id: string) {
-  
-  }
-
-  actualizarCurso() {
-  
-  }
-
-  eliminarCurso() {
-  
+  eliminarCurso(id: number) {
+    this.courseService.deleteCourse(id).subscribe({
+      next: () => {
+        console.log('Curso eliminado');
+        this.cargarCursos();
+      },
+      error: (err) => {
+        console.error('Error al eliminar curso:', err);
+      },
+    });
   }
 
   buscarCurso() {
-  
+
   }
 }
